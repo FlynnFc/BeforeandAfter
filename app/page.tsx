a6c5da9880da201af9b0fcac86055bcde96c6ebb
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-06-28 22:17:06 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,6 @@ import banner from "@/public/banner.png";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-full w-full  gap-36 ">
-      <Image
-        src={banner}
-        alt="bg"
-        draggable={false}
-        className="fixed -top-24 w-[90rem] select-none z-0"
-      />
       <Navbar />
       <Toaster position="bottom-center" />
       <section className="w-full z-10 h-full flex justify-center items-start">
