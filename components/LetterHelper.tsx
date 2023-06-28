fe9b32f7004362b011bc96ab0737317ea58dc222
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-06-28 17:13:18 +0100

    Update LetterHelper.tsx

--- components/LetterHelper.tsx
+++ components/LetterHelper.tsx
@@ -5,26 +5,26 @@ const LetterHelper = () => {
     <ul className="uppercase h-auto flex flex-col text-3xl overflow-hidden">
       <li>a</li>
       <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
-      <li>b</li>
+      <li>c</li>
+      <li>d</li>
+      <li>e</li>
+      <li>f</li>
+      <li>g</li>
+      <li>h</li>
+      <li>i</li>
+      <li>j</li>
+      <li>k</li>
+      <li>l</li>
+      <li>m</li>
+      <li>n</li>
+      <li>o</li>
+      <li>p</li>
+      <li>q</li>
+      <li>r</li>
+      <li>s</li>
+      <li>t</li>
+      <li>u</li>
+      <li>v</li>
     </ul>
   );
 };
